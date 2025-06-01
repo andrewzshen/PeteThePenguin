d2713c7ad5b035dcc2f7829c04ee58089607ff82
Andrew <andrewshenrocks@gmail.com>
2025-05-31 23:37:43 -0700

    riding works now should probably refactor and make it not gross but i will do it later

--- Assets/Scripts/Player/GroundRider.cs
+++ Assets/Scripts/Player/GroundRider.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class GroundRider : MonoBehaviour {
 
+    private Rigidbody rb;
+    private Vector3 forceFromGravity;
+    private Vector3 inputDirection;
+    private Vector2 moveInput;
+
     [SerializeField] private InputReader inputReader;
 
-    [Header("Height Spring")]
+    [Header("Ride Height Spring")]
     [SerializeField] private float rayCheckLength;
     [SerializeField] private float rideHeight;
-    [SerializeField] private float rideSpringStrength;
-    [SerializeField] private float rideSpringDampingFactor;
+    [SerializeField] private float rideHeightSpringStrength;
+    [SerializeField] private float rideHeightSpringDampingFactor;
     [SerializeField] private float fallForce;
 
-    [Header("Upright String")]
+    [Header("Upright Spring")]
     [SerializeField] private float uprightRotationSpringStrength;
     [SerializeField] private float uprightRotationSpringDampingFactor;
-
     private Quaternion lastTargetRotation;
 
     [Header("Movement")]
@@ -26,11 +30,8 @@ public class GroundRider : MonoBehaviour {
     [SerializeField] private float maxAcceleration;
     [SerializeField] private AnimationCurve accelerationFactorFromDot;
     [SerializeField] private AnimationCurve maxAccelerationFactorFromDot;
-    
-    private Vector2 moveInput;
 
-    private Vector3 inputDirection;
-    private Vector3 targetVelocity;
+    private Vector3 targetVelocity = Vector3.zero;
 
     [Header("Jumping")]
     [SerializeField] private float jumpForce;
@@ -45,8 +46,6 @@ public class GroundRider : MonoBehaviour {
     }
     private StateMachine<States> stateMachine;
 
-    private Rigidbody rb;
-
     private Raycaster raycaster;
 
     private bool isJumping;
@@ -59,6 +58,7 @@ public class GroundRider : MonoBehaviour {
         inputReader.SurfEvent += OnSurf;
 
         rb = GetComponent<Rigidbody>();
+        forceFromGravity = Physics.gravity * rb.mass;
 
         stateMachine = new StateMachine<States>(this);
         stateMachine.ChangeState(States.Grounded);
@@ -81,34 +81,35 @@ public class GroundRider : MonoBehaviour {
     private void FixedUpdate() {
         stateMachine.Driver.FixedUpdate.Invoke();
         HandleMovement();
-        MaintainUpright();
+        MaintainUprightRotation();
+        MaintainRideHeight();
     }
 
     private void Grounded_FixedUpdate() {
-        MaintainHeight();
+
     }
 
-    private void MaintainHeight() {
+    private void MaintainRideHeight() {
         bool nearGround = NearGround(out RaycastHit hitInfo);
 
         if(nearGround) {
             float distanceFromRideHeight = rideHeight - hitInfo.distance;
             float downVelocity = Vector3.Dot(Vector3.down, rb.linearVelocity);
 
-            float springForce = -distanceFromRideHeight * rideSpringStrength;
-            float dampingForce = downVelocity * rideSpringDampingFactor;
-            Vector3 force = (springForce - dampingForce) * Vector3.down;
+            float springForce = -distanceFromRideHeight * rideHeightSpringStrength;
+            float dampingForce = downVelocity * rideHeightSpringDampingFactor;
+            Vector3 rideForce = (springForce - dampingForce) * Vector3.down;
 
-            rb.AddForce(force);
+            rb.AddForce(rideForce - forceFromGravity);
 
             if(hitInfo.rigidbody != null) {
-                hitInfo.rigidbody.AddForceAtPosition(-force, hitInfo.point);
+                hitInfo.rigidbody.AddForceAtPosition(-rideForce, hitInfo.point);
             }
         }
     }
 
     private void HandleMovement() {
-        inputDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
+        inputDirection = new Vector3(moveInput.x, 0f, moveInput.y);
 
         if(inputDirection.magnitude > 1.0f) {
             inputDirection.Normalize();
@@ -125,27 +126,27 @@ public class GroundRider : MonoBehaviour {
         float modifiedMaxAcceleration = maxAcceleration * maxAccelerationFactorFromDot.Evaluate(dot);
         neededAcceleration = Vector3.ClampMagnitude(neededAcceleration, modifiedMaxAcceleration);
 
-        Vector3 force = Vector3.Scale(rb.mass * neededAcceleration, new Vector3(1f, 0f, 1f));
-        rb.AddForce(force);
+        Vector3 movementForce = Vector3.Scale(rb.mass * neededAcceleration, new Vector3(1f, 0f, 1f));
+        rb.AddForce(movementForce);
     }
 
-    private void MaintainUpright() {
+    private void MaintainUprightRotation() {
         Quaternion currentRotation = transform.rotation;
         Quaternion targetRotation;
 
         if(inputDirection.magnitude > 0.05f) {
-            // We only care about the horizontal component:
-            Vector3 flatLook = new Vector3(inputDirection.x, 0f, inputDirection.y).normalized;
             targetRotation = Quaternion.LookRotation(inputDirection, Vector3.up);
-
-            // Save the “last yaw” so we can hold it when inputDirection → 0
-            lastTargetRotation = Quaternion.Euler(0f, targetRotation.eulerAngles.y, 0f);
+            lastTargetRotation = targetRotation;
         } else {
-            // 2b) No input → just keep whatever yaw we had (no pitch/roll)
-            targetRotation = lastTargetRotation;
+            float lastYaw = lastTargetRotation.eulerAngles.y;
+            targetRotation = Quaternion.Euler(new Vector3(0f, lastYaw, 0f));
         }
 
-        Quaternion shortestRotation = CalculateShortestRotation(targetRotation, currentRotation);
+        Quaternion shortestRotation = targetRotation * Quaternion.Inverse(currentRotation);
+
+        if(Quaternion.Dot(targetRotation, currentRotation) < 0f) {
+            shortestRotation = new Quaternion(-shortestRotation.x, -shortestRotation.y, -shortestRotation.z, -shortestRotation.w);
+        }
 
         shortestRotation.ToAngleAxis(out float rotationDegrees, out Vector3 rotationAxis);
         float rotationRadians = rotationDegrees * Mathf.Deg2Rad;
@@ -153,37 +154,17 @@ public class GroundRider : MonoBehaviour {
 
         Vector3 springForce = rotationAxis * (rotationRadians * uprightRotationSpringStrength);
         Vector3 dampingForce = rb.angularVelocity * uprightRotationSpringDampingFactor;
-        Vector3 torque = springForce - dampingForce;
-        rb.AddTorque(torque);
+        Vector3 uprightTorque = springForce - dampingForce;
+        rb.AddTorque(uprightTorque);
     }
 
     private Quaternion CalculateTargetRotation() {
         return Quaternion.identity;
     }
 
-    private Quaternion CalculateShortestRotation(Quaternion targetRotation, Quaternion currentRotation) {
-        if(Quaternion.Dot(targetRotation, currentRotation) < 0f) {
-            return targetRotation * Quaternion.Inverse(QuatMultiply(currentRotation, -1f));
-        } else {
-            return targetRotation * Quaternion.Inverse(currentRotation);
-        }
-    }
-
-    private Quaternion QuatMultiply(Quaternion quat, float scalar) {
-        return new Quaternion(quat.x * scalar, quat.y * scalar, quat.z * scalar, quat.w * scalar);
-    }
-
     private bool NearGround(out RaycastHit rayHit) => Physics.Raycast(transform.position, Vector3.down, out rayHit, rayCheckLength);
 
-    private void OnMove(Vector2 input) {
-        moveInput = input;
-    }
-
-    private void OnJump(bool jump) {
-        isJumping = jump;
-    }
-
-    private void OnSurf(bool surf) {
-        isSurfing = surf;
-    }
+    private void OnMove(Vector2 input) => moveInput = input;
+    private void OnJump(bool jump) => isJumping = jump;
+    private void OnSurf(bool surf) => isSurfing = surf;
 }
